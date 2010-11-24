e2b780f6ebf509b5e04705c0568164c887267f5a
Jonathan <jon@acss.net.au>
2010-11-24 17:14:38 +1000

    Includ initial setup file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup (name="urwidx",
+       version="0.1a",
+       description="Urwid Extended",
+       author="Jonathan McManus",
+       author_email="jonathan@skyeindustries.com.au",
+       packages=['ux']
+      )
