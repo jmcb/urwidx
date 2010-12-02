59de3e57222f066451e05e1e452068e36fc35c36
Jonathan <jon@acss.net.au>
2010-12-02 19:58:01 +1000

    Move ListEditor(Walker) classes into layout.
    Include some code for demonstrating in-place editing and actually code
    in-place editing of lists.

--- ux/edittest.py
+++ ux/edittest.py
@@ -4,92 +4,6 @@ import app, form, urwid, layout
 import time
 header = None
 
-# Editor for in-place list alteration.
-class ListEditorWalker (urwid.SimpleListWalker):
-    def __init__ (self, contents, editor=urwid.Edit):
-        self.editors = [None for i in contents]
-        self.editor = editor
-        self.focus = 0
-
-        urwid.MonitoredList.__init__(self, contents)
-
-    def wipe_editors (self):
-        self.editors = [None for i in self.contents]
-
-    def _get_focus (self, pos):
-        if pos < 0: return None, None
-        if len(self.contents) <= pos: return None, None
-
-        return self._get_focus_editor(pos), pos
-
-    def _get_focus_editor (self, pos):
-        if self.editors[pos]:
-            return self.editors[pos]
-
-        def update_list (pos):
-            def f (editor, new_value):
-                if not self.editors[pos]:
-                    self.editors[pos] = None
-                self.contents[pos] = new_value
-                self._modified()
-            return f
-
-        editor = self.editor(edit_text=self.contents[pos])
-        urwid.connect_signal(editor, 'change', update_list(pos))
-        self.editors[pos] = editor
-
-        return editor
-
-    def set_focus (self, focus):
-        assert isinstance(focus, int)
-        self.focus = focus
-        self._modified()
-
-    def get_next (self, pos):
-        return self._get_focus(pos+1)
-
-    def get_prev (self, pos):
-        return self._get_focus(pos-1)
-
-    def get_focus (self):
-        if len(self.contents) == 0: return None, None
-        return self._get_focus_editor(self.focus), self.focus
-
-    def new (self, end=True):
-        if end == True:
-            self.contents.append("")
-            self.editors.append(None)
-            self.focus = len(self.contents)-1
-        else:
-            self.contents.insert(self.focus, "")
-            self.editors.append(None)
-
-    def snip (self):
-        del self.contents[self.focus]
-        self.wipe_editors()
-
-class ListEditor (urwid.ListBox):
-    def __init__ (self, to_edit, editor=urwid.Edit, walker=ListEditorWalker, meta_key="ctrl e", del_key="-", append_key="+", insert_key="insert"):
-        self.meta_key = meta_key
-        self.append_key = append_key
-        self.insert_key = insert_key
-        self.delete_key = del_key
-        self.looking_meta = False
-        self.walker = walker(to_edit, editor)
-        urwid.ListBox.__init__(self, self.walker)
-
-    def keypress (self, size, key):
-        if self.looking_meta and (key == self.append_key or key == self.append_key):
-            self.walker.new(key == self.append_key)
-            return
-        elif self.looking_meta and key == self.delete_key:
-            self.walker.snip()
-            return
-        else:
-            self.looking_meta = (key == self.meta_key)
-
-        return urwid.ListBox.keypress(self, size, key)
-
 class EditTestForm (form.Form):
     def OnInit (self):
         global header
@@ -98,9 +12,9 @@ class EditTestForm (form.Form):
 
         capt=urwid.Text("This is the edit caption: ")
         self.data_list = [str(("%s " % x) * 15).strip() for x in xrange(10)]
-        edit=ListEditor(self.data_list)
+        self.edit=layout.ListEditor(self.data_list)
 
-        col=urwid.Columns([('fixed', len(capt.text)+1, capt), urwid.BoxAdapter(edit, 10)])
+        col=urwid.Columns([('fixed', len(capt.text)+1, capt), urwid.BoxAdapter(self.edit, 10)])
 
         self.body = urwid.Filler(col)
 
@@ -120,7 +34,10 @@ def main ():
                ('understand', 'black', 'dark green')]
 
     app = EditTestApp(palette=palette)
-    app.MainLoop()
+    try:
+        app.MainLoop()
+    except KeyboardInterrupt:
+        print app.GetTopForm().data_list
 
 if __name__=="__main__":
     main ()
--- ux/layout.py
+++ ux/layout.py
@@ -12,3 +12,99 @@ class OffsetOverlay(urwid.Overlay):
 class SizedFrame (urwid.BoxAdapter):
     def __init__ (self, height, body, header=None, footer=None, focus_part='body'):
         urwid.BoxAdapter.__init__(self, urwid.Frame(body, header, footer, focus_part), height)
+
+# Walker that interfaces on a list of string values, providing editors for them.
+# Support in-place editing by setting in_place=True.
+class ListEditorWalker (urwid.SimpleListWalker):
+    def __init__ (self, contents, editor=urwid.Edit, in_place=True):
+        self.to_update = contents
+        self.editors = [None for i in contents]
+        self.editor = editor
+        self.focus = 0
+
+        urwid.MonitoredList.__init__(self, contents)
+
+        if in_place:
+            urwid.connect_signal(self, "modified", self.update_list)
+
+    def update_list (self):
+        self.to_update[:] = list(self)
+
+    def wipe_editors (self):
+        self.editors = [None for i in self.contents]
+
+    def _get_focus (self, pos):
+        if pos < 0: return None, None
+        if len(self.contents) <= pos: return None, None
+
+        return self._get_focus_editor(pos), pos
+
+    def _get_focus_editor (self, pos):
+        if self.editors[pos]:
+            return self.editors[pos]
+
+        def update_list (pos):
+            def f (editor, new_value):
+                if not self.editors[pos]:
+                    self.editors[pos] = None
+                self.contents[pos] = new_value
+                self._modified()
+            return f
+
+        editor = self.editor(edit_text=self.contents[pos])
+        urwid.connect_signal(editor, 'change', update_list(pos))
+        self.editors[pos] = editor
+
+        return editor
+
+    def set_focus (self, focus):
+        assert isinstance(focus, int)
+        self.focus = focus
+        self._modified()
+
+    def get_next (self, pos):
+        return self._get_focus(pos+1)
+
+    def get_prev (self, pos):
+        return self._get_focus(pos-1)
+
+    def get_focus (self):
+        if len(self.contents) == 0: return None, None
+        return self._get_focus_editor(self.focus), self.focus
+
+    def new (self, end=True):
+        if end == True:
+            self.contents.append("")
+            self.editors.append(None)
+            self.focus = len(self.contents)-1
+        else:
+            self.contents.insert(self.focus, "")
+            self.editors.append(None)
+
+    def snip (self):
+        del self.contents[self.focus]
+        self.wipe_editors()
+
+# An interface to ListEditorWalker.
+class ListEditor (urwid.ListBox):
+    def __init__ (self, to_edit, editor=urwid.Edit, walker=ListEditorWalker, meta_key="ctrl e", del_key="-", append_key="+", insert_key="insert"):
+        self.to_edit = to_edit
+        self.meta_key = meta_key
+        self.append_key = append_key
+        self.insert_key = insert_key
+        self.delete_key = del_key
+        self.looking_meta = False
+        self.walker = walker(self.to_edit, editor)
+        urwid.ListBox.__init__(self, self.walker)
+
+    def keypress (self, size, key):
+        if self.looking_meta and (key == self.append_key or key == self.append_key):
+            self.walker.new(key == self.append_key)
+            return
+        elif self.looking_meta and key == self.delete_key:
+            self.walker.snip()
+            return
+        else:
+            self.looking_meta = (key == self.meta_key)
+
+        return urwid.ListBox.keypress(self, size, key)
